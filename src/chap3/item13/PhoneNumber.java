37f9aa797a4ef1f42aa7d90fd55958f3f49c335d
DevJun <guswns1659@gmail.com>
2020-10-30 16:26:24 +0900

    Feat : 아이템13. 모든 필드가 기본타입이거나 불변 객체를 참조하는 경우의 clone() 정의
    - 코드 13-1 가변 상태를 참조하지 않는 클래스용 clone 메서드

---
+++ src/chap3/item13/PhoneNumber.java
@@ -0,0 +1,31 @@
+package chap3.item13;
+
+public final class PhoneNumber implements Cloneable {
+
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "지역코드");
+        this.prefix = rangeCheck(prefix, 999, "프리픽스");
+        this.lineNum = rangeCheck(lineNum, 999, "가입자 번호");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max) {
+            throw new IllegalArgumentException(arg + ": " + val);
+        }
+        return (short) val;
+    }
+
+    /** 모든 필드가 기본타입이거나 불변 객체를 참조하는 경우의 clone() 정의
+     *
+     */
+    @Override
+    public PhoneNumber clone() {
+        try {
+            return (PhoneNumber) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // 일어날 수 없는 일이다.
+        }
+    }
+}
