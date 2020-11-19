cab4664b5710801dacf5f2fb8b48fcb7871dc9eb
guswns1659 <guswns1659@gmail.com>
2020-11-19 19:48:26 +0900

    item11 정리

--- src/chap3/item11/AutoValueMain.java
+++ src/chap3/item11/AutoValueMain.java
@@ -3,7 +3,8 @@ package chap3.item11;
 public class AutoValueMain {
 
     public static void main(String[] args) {
-        Item item = Item.builder().price(10000).name("name").build();
-        System.out.println(item);
+        // Item item = Item.builder().price(10000).name("name").build();
+//        System.out.println(item);
+
     }
 }
